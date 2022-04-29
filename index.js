37d21864ce8b7a7b584b32d6c7823efdb32de8ee
Emmanuel <emmopk11@outlook.com>
2022-04-29 14:00:23 +0200

    uibuild

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
+app.use(express.static("build"));
+
 //======== GET ======
 app.get("/api/persons", (req, res) => {
   res.status(200).json(data);
