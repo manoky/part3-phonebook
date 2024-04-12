25ae2e33d871f4a9d3cda1398b5b50eb53a290f4
Emmanuel <emmopk11@outlook.com>
2024-04-12 10:24:03 +0200

    build ui

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # part3-phonebook
+Frontend-URL:https://phonebook-d667c0c0516d.herokuapp.com/
+Backend-URL:https://phonebook-d667c0c0516d.herokuapp.com/api/persons
--- index.js
+++ index.js
@@ -5,29 +5,6 @@ const connectToDB = require("./mongo")
 const Person = require("./models/person")
 const errorHandler = require("./middlewares/errorHandler")
 
-// let persons = [
-//   {
-//     id: 1,
-//     name: "Arto Hellas",
-//     number: "040-123456",
-//   },
-//   {
-//     id: 2,
-//     name: "Ada Lovelace",
-//     number: "39-44-5323523",
-//   },
-//   {
-//     id: 3,
-//     name: "Dan Abramov",
-//     number: "12-43-234345",
-//   },
-//   {
-//     id: 4,
-//     name: "Mary Poppendieck",
-//     number: "39-23-6423122",
-//   },
-// ]
-
 const app = express()
 
 app.use(cors())
